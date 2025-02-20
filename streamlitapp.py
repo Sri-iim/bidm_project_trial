c48964767e9646ccc7c48d78a20f1dcd39275080
Sri-iim <embadta24012@iimsirmaur.ac.in>
2025-02-20 09:43:08 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-from fbprophet import Prophet
+from prophet import Prophet
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
