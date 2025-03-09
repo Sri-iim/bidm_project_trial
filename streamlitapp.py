26aa59839cea604040770f313758967ffcab29f8
Sri-iim <embadta24012@iimsirmaur.ac.in>
2025-03-09 19:46:37 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -261,6 +261,7 @@ y_class = df["AQI_Label"].values.ravel()  # ✅ Convert to 1D array
 X_train, X_test, y_train, y_test = train_test_split(X_class, y_class, test_size=0.2, random_state=42)
 
 # Train model
+log_reg = LogisticRegression(max_iter=500, solver='saga') 
 log_reg = LogisticRegression()
 log_reg.fit(X_train, y_train)
 
